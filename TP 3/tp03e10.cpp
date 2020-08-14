fa1735ef7e0614aace9e827fb1e8c7d13ada2fd3
Menoni Matias <65802752+rookie623@users.noreply.github.com>
2020-08-14 01:24:33 -0300

    Resuelto

--- TP 3/tp03e10.cpp
+++ TP 3/tp03e10.cpp
@@ -1,12 +1,12 @@
 /*
-    10) Hacer un programa para ingresar una lista de 10 n˙meros, luego informar el m·ximo y
-        el mÌnimo.
+    10) Hacer un programa para ingresar una lista de 10 n√∫meros, luego informar el m√°ximo y
+        el m√≠nimo.
 
-        Ejemplo: 10, 20, -5, 30,-15, 5, 42, 0, 22, -13. Se listar· M·ximo 42 MÌnimo -15.
-        Ejemplo: 10, 20, 5, 30, 15, 5, 42, 8, 22, 13. Se listar· M·ximo 42 MÌnimo 5.
-        Ejemplo: -10, -20, -5, -30, -15, -12, -42, -8, -22, -13. Se listar· M·ximo -5 MÌnimo -42.
-        Observe que los tres ejemplos dejan en claro que la idea de que el m·ximo
-        ìseguramenteî es un positivo y el mÌnimo ìseguramenteî es un negativo, queda
+        Ejemplo: 10, 20, -5, 30,-15, 5, 42, 0, 22, -13. Se listar√° M√°ximo 42 M√≠nimo -15.
+        Ejemplo: 10, 20, 5, 30, 15, 5, 42, 8, 22, 13. Se listar√° M√°ximo 42 M√≠nimo 5.
+        Ejemplo: -10, -20, -5, -30, -15, -12, -42, -8, -22, -13. Se listar√° M√°ximo -5 M√≠nimo -42.
+        Observe que los tres ejemplos dejan en claro que la idea de que el m√°ximo
+        ‚Äúseguramente‚Äù es un positivo y el m√≠nimo ‚Äúseguramente‚Äù es un negativo, queda
         totalmente descartada.
 */
 #include <iostream>
@@ -15,7 +15,7 @@ using namespace std;
 int main()
 {	
 	int i; 
-	int j = 0, l;
+	int j = 0, l = 0;
 	int num;
 	int max, maxPos;
 	int min, minPos;
@@ -31,24 +31,18 @@ int main()
 			maxPos = i;
 			j++;
 		}
-		else
-		{
-			if(num > max)
+		else if(num > max)
 			{
 				max = num;
 				maxPos = i;
 			}
-			else
-			{
-				if(!l)
+			else if(!l)
 				{
 					min = num;
 					minPos = i;
 					l++;
 				}
-				else
-				{
-					if(num < min)
+				else if(num < min)
 					{
 						min = num;
 						minPos = i;
@@ -59,7 +53,6 @@ int main()
 	}
 
 	cout << "El maximo encontrado es " << max << " posicion " << maxPos + 1 << endl;
-	
 	cout << "El minimo encontrado es " << min << " posicion " << minPos + 1 << endl;
 
 	return 0;
