e8f5f492b26f73f9600186d80612afb3946d97f1
Leiitern <70219388+Leiitern@users.noreply.github.com>
2020-08-25 13:10:28 -0300

    Update tp03e09.cpp
    El código esta dando mal porque no me da lo que me esta pidiendo para eso deberias iniciar la variable maximo en 0 y la variable pos en 0 tambien asi te tira el maximo y la posicion

--- TP 3/tp03e09.cpp
+++ TP 3/tp03e09.cpp
@@ -1,9 +1,9 @@
 /*
-    9) Hacer un programa para ingresar una lista de 10 n˙meros, luego informar el m·ximo y la posiciÛn del m·ximo en la lista.
-       En caso de ìempatesî considerar la primera apariciÛn.
+    9) Hacer un programa para ingresar una lista de 10 n√∫meros, luego informar el m√°ximo y la posici√≥n del m√°ximo en la lista.
+       En caso de ‚Äúempates‚Äù considerar la primera aparici√≥n.
 
-       Ejemplo: 5, -10, 20, 8, 25, 13, 35, -8, -5, 20. Se listar· M·ximo 35 PosiciÛn 7.
-       Ejemplo: 5, -10, 20, 8, 25, 13, 55, -8, 55, 20. Se listar· M·ximo 55 PosiciÛn 7.
+       Ejemplo: 5, -10, 20, 8, 25, 13, 35, -8, -5, 20. Se listar√° M√°ximo 35 Posici√≥n 7.
+       Ejemplo: 5, -10, 20, 8, 25, 13, 55, -8, 55, 20. Se listar√° M√°ximo 55 Posici√≥n 7.
 */
 #include <iostream>
 using namespace std;
@@ -11,8 +11,8 @@ using namespace std;
 int main()
 {
     int num;
-    int maximo;
-    int pos;
+    int maximo=0;
+    int pos=0;
     int i;
 
     cout << "A continuacion se le solicitara ingresar 10 numeros:" << endl << endl;
